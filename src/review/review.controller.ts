e467706a6bebaf25bd4ec83ff1df1749439367d8
mariama4 <mariama4@mail.ru>
2023-03-31 18:00:29 +0700

    add updates and docker

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,22 +7,30 @@ import {
 	HttpStatus,
 	Param,
 	Post,
+	UseGuards,
+	UsePipes,
+	ValidationPipe,
 } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
+import { JwtAuthGuard } from '../auth/guards/jwt.guards';
+import { UserEmail } from '../decorators/user-email.decorator';
+import { IdValidationPipe } from '../pipes/ad-validation.pipe';
 
 @Controller('review')
 export class ReviewController {
 	constructor(private readonly reviewService: ReviewService) {}
 
+	@UsePipes(new ValidationPipe())
 	@Post('create')
 	async create(@Body() dto: CreateReviewDto) {
 		return this.reviewService.create(dto);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
+	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deleteDoc = await this.reviewService.delete(id);
 		if (!deleteDoc) {
 			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
@@ -31,7 +39,10 @@ export class ReviewController {
 	}
 
 	@Get('byProduct/:productId')
-	async getByProduct(@Param('productId') productId: string) {
+	async getByProduct(
+		@Param('productId', IdValidationPipe) productId: string,
+		@UserEmail() email: string,
+	) {
 		return this.reviewService.findByProductId(productId);
 	}
 }
